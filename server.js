97cd41790e3ec437090a832edc29a88dcd0a137c
Waqas nazir <vickydev31113@gmail.com>
2021-02-26 23:49:07 +0500

    challenge completed huhhhhhhh

--- server.js
+++ server.js
@@ -2,4 +2,32 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express')
+var app = express()
+var fs = require('fs')
+
 var jsonData = { count: 12, message: 'hey' };
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html', (err) => {
+//         if (err) {
+//             res.status(500).send(err)
+//         }
+//     })
+// })
+app.get('/', (req, res) => {
+    fs.readFile('index.html', (err, buffer) => {
+        var html = buffer.toString();
+        res.setHeader('Content-Type', 'text/html')
+        res.send(html);
+    })
+})
+
+app.get('/data', (req, res) => {
+    res.json(jsonData)
+})
+
+var port = 4000;
+app.listen(port, () => {
+    console.log(`server is up and running on http://localhost:${port}`)
+})
