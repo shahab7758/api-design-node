0b2d8618f64dac73a8d79a5d3a1bb0eceea82390
Waqas nazir <vickydev31113@gmail.com>
2021-02-26 22:50:22 +0500

    create server and simple route

---
+++ server.js
@@ -0,0 +1,5 @@
+// TODO: create a basic server with express
+// that will send back the index.html file on a GET request to '/'
+// it should then send back jsonData on a GET to /data
+
+var jsonData = { count: 12, message: 'hey' };
